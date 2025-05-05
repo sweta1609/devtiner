f076321cae8d496f44e598ca981f426555bf764b
Sweta Kumari <sweta.kumari@nirmitee.io>
2025-05-05 14:11:43 +0530

    adding initial commit

---
+++ Readme.md
@@ -0,0 +1,5 @@
+nodemon refreshes server automatically if you do any changes
+
+
+in package json in script you can add 
+start:"nodemon app/src.js"
---
+++ src/app.js
@@ -0,0 +1,8 @@
+   const express = require('express');
+   const app = express();
+   app.use("/test",(req,res)=>{
+    res.send("hello from server")
+   })
+   app.listen(3000,()=>{
+    console.log("server is calling")
+   });
