be0050ba1507bff3f701141252bb1f84b7d76875
Sweta Kumari <sweta.kumari@nirmitee.io>
2025-05-07 01:05:04 +0530

    aading for  app.js

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
    const express = require('express');
    const app = express();
-   app.use("/test",(req,res)=>{
-    res.send("hello from server")
-   })
+   app.use("/user",(req,res,next)=>{
+      console.log("handling the route user");
+      res.send("Response");
+      next()
+   },
+(req,res)=>{
+   console.log("handling response");
+   res.send("2nd response");
+})
    app.listen(3000,()=>{
     console.log("server is calling")
    });
