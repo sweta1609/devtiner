bb23712a12e96f71c44ed7ea87bafffcf2b81898
Sweta Kumari <sweta.kumari@nirmitee.io>
2025-05-07 23:15:05 +0530

    multipple route handlers

--- src/app.js
+++ src/app.js
@@ -2,12 +2,20 @@
    const app = express();
    app.use("/user",(req,res,next)=>{
       console.log("handling the route user");
-      res.send("Response");
       next()
+      res.send("Response");
    },
 (req,res)=>{
    console.log("handling response");
    res.send("2nd response");
+},
+(req,res)=>{
+   console.log("handling response");
+   res.send("3rd response");
+},
+(req,res)=>{
+   console.log("handling response");
+   res.send("4th response");
 })
    app.listen(3000,()=>{
     console.log("server is calling")
