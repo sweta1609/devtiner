c040e60448e9a41052444e33da900f54527f72b9
Sweta Kumari <sweta.kumari@nirmitee.io>
2025-05-13 23:19:17 +0530

    encrypting password

--- src/app.js
+++ src/app.js
@@ -1,20 +1,47 @@
 const express = require('express');
 const app = express();
+const {validateSignUpData}=require("./utils/validation")
 const connectDB=require("./config/database")
 const User=require("./models/user")
+const bcrypt = require("bcrypt")
 
 app.use(express.json());
   
 app.post("/signup",async(req,res)=>{
+
   
-   const user=new User(req.body);
-   try{await user.save();
+   try{
+        validateSignUpData(req)
+        const {firstName,lastName,emailId,password}=req.body;
+        
+         const passwordHash =await  bcrypt.hash(password,10)
+         console.log(passwordHash)
+          const user=new User({firstName,lastName,emailId,password:passwordHash});
+      await user.save();
    res.send("user added successfully")
 }catch(err){
    console.log(err)}
    res.status(400).send("error saving middleware")
    
 
+})
+
+app.post("/login",async(req,res)=>{
+   try{
+      const {emailId,password}=req.body;
+      const user = await User.findOne({emailId:emailId});
+      if(!user){
+         throw new Error("Invalid credentials")
+      }
+      const isPasswordValid=bcrypt.compare(password,user.password)
+      if(isPasswordValid){
+         res.send("Login successfull")
+      }else{
+         throw new Error("password is not correct")
+      }
+   }catch(error){
+         res.status(400).send("error loging in")
+   }
 })
 
 app.get("/user",async(req,res)=>{
