53e3c24e4797b37c12412cc26b7c79b107d69637
Sweta Kumari <sweta.kumari@nirmitee.io>
2025-05-25 09:52:16 +0530

    adding feed api

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,6 +3,7 @@ const { userAuth } = require("../middleware/auth")
 const ConnectionRequest = require("../models/connectionRequest")
 const ConnectionRequestModel = require("../models/connectionRequest")
 const userRouter = express.Router()
+const User = require("../models/user")
 
 userRouter.get("/user/requests/received",userAuth,async(req,res)=>{
     try{
@@ -43,6 +44,35 @@ userRouter.get("/user/connections",userAuth,async(req,res)=>{
     }catch(err){
         res.status(400).json({message:err.message})
     }
+})
+
+userRouter.get("/feed",userAuth,async(req,res)=>{
+  try{
+    const loggedInUser = req.user;
+
+    const page=parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    const skip = (page-1)*limit;
+    const connectionrequest = await ConnectionRequest.find({
+        $or:[{fromUserId:loggedInUser._id},{toUserId:loggedInUser._id}]
+    }).select("fromUserId toUserId")
+
+    const hideUsersFromFeed = new Set();
+    connectionrequest.forEach((req)=>{
+        hideUsersFromFeed.add(req.fromUserId.toString())
+        hideUsersFromFeed.add(req.toUserId.toString())
+    })
+    console.log(hideUsersFromFeed)
+    const users= await User.find({
+        $and:[
+            {_id:{$nin:Array.from(hideUsersFromFeed)}},
+            {_id:{$ne:loggedInUser._id}}
+        ]
+    }).select("firstName lastName").skip(skip).limit(limit)
+    res.send(users)
+  }catch(error){
+    res.status(400).res({message:error.message})
+  }
 })
 
 module.exports = userRouter
